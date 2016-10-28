ef1aa7a47964927beaa4e98f5181df5fcea21dd9
Rig Perez <rlperez@udel.edu>
2016-10-28 01:03:40 -0400

    Complete chapter 3 exercise 1

---
+++ clojure-noob/src/chapter_3/one.clj
@@ -0,0 +1,18 @@
+(def statement (str "Now that there is the Tec-9, a crappy spray gun from South Miami. " 
+                    "This gun is advertised as the most popular gun in American crime. " 
+                    "Do you believe that shit? " 
+                    "It actually says that in the little book that comes with it: the most popular gun in American crime. " 
+                    "Like they're actually proud of that shit."))
+(println statement)
+
+(def a-vector (vector 1 2 3 4 5 6 7 8 9 0))
+(println a-vector)
+
+(def a-list (list 1 2 3 4 5 6 7 8 9 0))
+(println a-list)
+
+(def a-hash-map (hash-map :a 1 :b 2 :c 3))
+(println a-hash-map)
+
+(def a-hash-set (hash-set 1 1 2 3 4))
+(println a-hash-set)
